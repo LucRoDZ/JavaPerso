78f7879cca605605f762f0e2a68d618e7af38084
lucas.zivan <lucas.zivan@epita.fr>
2025-02-04 03:17:49 +0100

    feat: Hello World Exercice

---
+++ 2-syntax/helloWorld/helloWorld/src/main/java/fr/epita/assistants/helloworld/Main.java
@@ -0,0 +1,10 @@
+package fr.epita.assistants.helloworld;
+
+public class Main {
+    public static void main(String[] args) {
+        HelloWorld hello = new HelloWorld();
+        hello.printHelloWorldErr();
+        hello.printHelloWorld();
+    }
+}
+
