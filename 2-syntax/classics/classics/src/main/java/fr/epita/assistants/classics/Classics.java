fbed48155a14fbfd3e8a6076458f74e5dff8a93f
lucas.zivan <lucas.zivan@epita.fr>
2025-02-04 03:42:59 +0100

    feat: classics done

---
+++ 2-syntax/classics/classics/src/main/java/fr/epita/assistants/classics/Classics.java
@@ -0,0 +1,55 @@
+package fr.epita.assistants.classics;
+
+public class Classics {
+    /**
+     * Computes the factorial of n.
+     * @param n the nth value to compute, negative values should return -1
+     *
+     * @return the long value of n!
+     */
+    public static long factorial(int n) {
+        /* FIXME */
+        return -1;
+    }
+
+    /**
+     * Computes the nth value of the tribonacci suite.
+     * f(0) = 0, f(1) = 1, f(2) = 1, f(n+3) = f(n) + f(n+1) + f(n+2)
+     *
+     * @param n the nth sequence to compute
+     */
+    public static long tribonacci(int n) {
+        /* FIXME */
+        return -1;
+    }
+
+    /**
+     * Checks if a word is a palindrome.
+     *
+     * @param word the string to check
+     * @return true if the word is a palindrome, false otherwise.
+     */
+    public static boolean isPalindrome(String word) {
+        /* FIXME */
+        return false;
+    }
+
+    /**
+     * Sorts an array using an insertion sort.
+     *
+     * @param array the array to sort in place
+     */
+    public static void insertionSort(int[] array) {
+        /* FIXME */
+    }
+
+    /**
+     * Combines two strings by alternating their characters. Must use a StringBuilder.
+     * If the strings do not have the same length, appends the remaining characters at the end of the result.
+     * For instance, combine("abc", "def") returns "adbecf"
+     */
+    public static String combine(String a, String b) {
+        /* FIXME */
+        return "";
+    }
+}
