bf308ae617f782313ef2bd8cb406403ed6d79caf
lucas.zivan <lucas.zivan@epita.fr>
2025-02-04 01:11:29 +0100

    feat: Basics need for start working

---
+++ 1-bootstrap/triad/triad/src/main/java/fr/epita/assistants/triad/WriteGettersAndConstructorForMe.java
@@ -0,0 +1,29 @@
+package fr.epita.assistants.triad;
+
+public class WriteGettersAndConstructorForMe {
+    private final String name;
+    private final String firstName;
+    private final String lastName;
+
+    private final String random;
+
+    private final String anotherOne;
+
+    private final String anotherOne2;
+
+    private final String anotherOne3;
+
+    private final String xyz;
+
+    private final int x;
+
+    private final double y;
+
+    private final float z;
+
+    private final Boolean b;
+
+    private final boolean c;
+
+    // FIXME: Add a constructor with all the parameters and all the getters
+}
