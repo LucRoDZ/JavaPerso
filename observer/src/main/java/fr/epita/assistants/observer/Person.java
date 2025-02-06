8d8729a98942bc7eceddb56d4f642bbd32f2ebfd
Lucas Zivan <lucaszivan86@gmail.com>
2025-02-06 16:24:51 +0100

    Update Person.java

--- observer/src/main/java/fr/epita/assistants/observer/Person.java
+++ observer/src/main/java/fr/epita/assistants/observer/Person.java
@@ -1,13 +1,29 @@
 package fr.epita.assistants.observer;
 
-public class Person {
+public class Person implements Observable.Observer<Swimmer> {
     private final String name;
 
+    /**
+     * Constructor takes the name of the Person.
+     * When a Person is created, display:
+     * "<person's name> is taking a walk on the beach."
+     */
     public Person(String name) {
         this.name = name;
+        System.out.println(name + " is taking a walk on the beach.");
     }
 
-    public String getName() {
-        return name;
+    /**
+     * React to events from a Swimmer.
+     *
+     * @param swimmer The Swimmer that triggered the event.
+     */
+    @Override
+    public void onEvent(Swimmer swimmer) {
+        // If a swimmer is waving, the person waves back:
+        if (swimmer.getStatus() == SwimmerStatus.WAVING) {
+            System.out.println(name + ": Waves back at " + swimmer.getName() + ".");
+        }
+        // Other statuses (OK, DROWNING, TOO_FAR) do not require a Person’s reaction.
     }
 }
