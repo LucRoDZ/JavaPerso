3cd22a782ef0424bda3af5e07bfd08825730ee10
Lucas Zivan <lucaszivan86@gmail.com>
2025-02-06 16:24:28 +0100

    Update Lifeguard.java

--- observer/src/main/java/fr/epita/assistants/observer/Lifeguard.java
+++ observer/src/main/java/fr/epita/assistants/observer/Lifeguard.java
@@ -1,33 +1,36 @@
 package fr.epita.assistants.observer;
 
-/**
- * Lifeguard class that observes SwimmerStatus changes.
- */
-public class Lifeguard implements Observable.Observer<SwimmerStatus> {
+public class Lifeguard implements Observable.Observer<Swimmer> {
+    private final String name;
 
-    private String name;
-
+    /**
+     * Constructor takes the name of the Lifeguard.
+     * When a Lifeguard is created, display:
+     * "<lifeguard's name> begins to keep an eye on the swimmers."
+     */
     public Lifeguard(String name) {
         this.name = name;
+        System.out.println(name + " begins to keep an eye on the swimmers.");
     }
 
+    /**
+     * Reacts to events from a Swimmer.
+     *
+     * @param swimmer The Swimmer that triggered the event.
+     */
     @Override
-    public void onEvent(SwimmerStatus event) {
-        // React based on the event (status of the swimmer)
-        switch (event) {
-            case DROWNING:
-                System.out.println(name + ": The swimmer is drowning! I'm coming to help!");
-                break;
-            case TOO_FAR:
-                System.out.println(name + ": The swimmer is too far away! Let me check the situation.");
-                break;
-            case WAVING:
-                System.out.println(name + ": The swimmer is waving, they might need assistance.");
-                break;
-            case OK:
-            default:
-                System.out.println(name + ": The swimmer looks fine. No action needed right now.");
-                break;
+    public void onEvent(Swimmer swimmer) {
+        SwimmerStatus currentStatus = swimmer.getStatus();
+
+        // If a swimmer is drowning:
+        if (currentStatus == SwimmerStatus.DROWNING) {
+            System.out.println(name + ": I will save you " + swimmer.getName() + "!");
         }
+        // If a swimmer is too far:
+        else if (currentStatus == SwimmerStatus.TOO_FAR) {
+            System.out.println(name + ": " + swimmer.getName() + "! You are too far, come back!");
+        }
+        // Other statuses are not specifically mentioned for Lifeguard's reaction
+        // (OK, WAVING, etc.), so the lifeguard does nothing for them in this example.
     }
 }
