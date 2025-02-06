c991466a8bc0cad0239ea0913ae8644394d7dbe9
Lucas Zivan <lucaszivan86@gmail.com>
2025-02-06 16:23:44 +0100

    Update Swimmer.java

--- observer/src/main/java/fr/epita/assistants/observer/Swimmer.java
+++ observer/src/main/java/fr/epita/assistants/observer/Swimmer.java
@@ -3,63 +3,87 @@ package fr.epita.assistants.observer;
 import java.util.HashSet;
 import java.util.Set;
 
-public class Swimmer implements Observable<SwimmerStatus> {
+public class Swimmer implements Observable<Swimmer> {
+    // 1) A set of observers watching this Swimmer:
+    private final Set<Observer<Swimmer>> observers = new HashSet<>();
 
-    private final Set<Observer<SwimmerStatus>> observers = new HashSet<>();
-
-    // Current status of the Swimmer
-    private SwimmerStatus status = SwimmerStatus.OK;
-
-    // A simple name field to identify the Swimmer
+    // 2) The swimmer's name:
     private final String name;
 
+    // 3) The swimmer's status, defaulting to OK:
+    private SwimmerStatus status = SwimmerStatus.OK;
+
+    /**
+     * Constructor takes the name of the Swimmer.
+     * When a Swimmer is created, display "<swimmer's name> goes into the sea."
+     */
     public Swimmer(String name) {
         this.name = name;
+        System.out.println(name + " goes into the sea.");
     }
 
     /**
-     * Change the swimmer's status and notify observers about it.
-     * @param newStatus The new status of the Swimmer.
+     * @return The name of the Swimmer.
      */
-    public void setStatus(SwimmerStatus newStatus) {
-        this.status = newStatus;
-        // Notify all observers of the change
-        fire(newStatus);
-    }
-
-    public SwimmerStatus getStatus() {
-        return status;
-    }
-
     public String getName() {
         return name;
     }
 
-    // ----------------------------------------------------
-    // Methods from Observable
-    // ----------------------------------------------------
+    /**
+     * @return The current status of the Swimmer.
+     */
+    public SwimmerStatus getStatus() {
+        return status;
+    }
+
+    /**
+     * Sets the Swimmer’s status and notifies observers.
+     * Also prints specific messages if the Swimmer is drowning or waving.
+     *
+     * @param newStatus The new status for this Swimmer.
+     */
+    public void setStatus(SwimmerStatus newStatus) {
+        this.status = newStatus;
+
+        // Print additional messages depending on the status:
+        if (newStatus == SwimmerStatus.DROWNING) {
+            System.out.println(name + ": I'm drowning, help!!");
+        } else if (newStatus == SwimmerStatus.WAVING) {
+            System.out.println(name + ": Waves towards the shore");
+        }
+
+        // Notify all observers that THIS Swimmer has changed status
+        fire(this);
+    }
+
+    // -------------------------------------------------------------
+    // Implementing the methods from Observable<Swimmer>
+    // -------------------------------------------------------------
     @Override
-    public Set<Observer<SwimmerStatus>> getObservers() {
+    public Set<Observer<Swimmer>> getObservers() {
         return observers;
     }
 
     @Override
-    public void register(Observer<SwimmerStatus>... observers) {
-        for (Observer<SwimmerStatus> obs : observers) {
-            this.observers.add(obs);
+    public void register(Observer<Swimmer>... observers) {
+        for (Observer<Swimmer> observer : observers) {
+            this.observers.add(observer);
         }
     }
 
     @Override
-    public void unregister(Observer<SwimmerStatus> observer) {
+    public void unregister(Observer<Swimmer> observer) {
         this.observers.remove(observer);
     }
 
+    /**
+     * Notifies every registered observer by calling onEvent(...) 
+     * with THIS Swimmer as the event object.
+     */
     @Override
-    public void fire(SwimmerStatus event) {
-        // Notify every registered observer with the event
-        for (Observer<SwimmerStatus> obs : observers) {
-            obs.onEvent(event);
+    public void fire(Swimmer event) {
+        for (Observer<Swimmer> observer : observers) {
+            observer.onEvent(event);
         }
     }
 }
