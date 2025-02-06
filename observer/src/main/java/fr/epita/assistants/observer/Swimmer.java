38088d9baf0e8224a1cfdf74ca4c5746cfcdc1b8
Lucas Zivan <lucaszivan86@gmail.com>
2025-02-06 16:17:07 +0100

    Update Swimmer.java last

--- observer/src/main/java/fr/epita/assistants/observer/Swimmer.java
+++ observer/src/main/java/fr/epita/assistants/observer/Swimmer.java
@@ -1,29 +1,65 @@
 package fr.epita.assistants.observer;
 
-public class Lifeguard implements Observable.Observer<SwimmerStatus> {
+import java.util.HashSet;
+import java.util.Set;
 
+public class Swimmer implements Observable<SwimmerStatus> {
+
+    private final Set<Observer<SwimmerStatus>> observers = new HashSet<>();
+
+    // Current status of the Swimmer
+    private SwimmerStatus status = SwimmerStatus.OK;
+
+    // A simple name field to identify the Swimmer
     private final String name;
 
-    public Lifeguard(String name) {
+    public Swimmer(String name) {
         this.name = name;
     }
 
+    /**
+     * Change the swimmer's status and notify observers about it.
+     * @param newStatus The new status of the Swimmer.
+     */
+    public void setStatus(SwimmerStatus newStatus) {
+        this.status = newStatus;
+        // Notify all observers of the change
+        fire(newStatus);
+    }
+
+    public SwimmerStatus getStatus() {
+        return status;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    // ----------------------------------------------------
+    // Methods from Observable
+    // ----------------------------------------------------
     @Override
-    public void onEvent(SwimmerStatus event) {
-        switch (event) {
-            case DROWNING:
-                System.out.println(name + " => The swimmer is drowning! I'm rescuing them!");
-                break;
-            case TOO_FAR:
-                System.out.println(name + " => The swimmer is too far. I'm signaling them to come closer.");
-                break;
-            case WAVING:
-                System.out.println(name + " => The swimmer is waving. Checking if they need help!");
-                break;
-            case OK:
-            default:
-                System.out.println(name + " => The swimmer is OK. No action needed right now.");
-                break;
+    public Set<Observer<SwimmerStatus>> getObservers() {
+        return observers;
+    }
+
+    @Override
+    public void register(Observer<SwimmerStatus>... observers) {
+        for (Observer<SwimmerStatus> obs : observers) {
+            this.observers.add(obs);
+        }
+    }
+
+    @Override
+    public void unregister(Observer<SwimmerStatus> observer) {
+        this.observers.remove(observer);
+    }
+
+    @Override
+    public void fire(SwimmerStatus event) {
+        // Notify every registered observer with the event
+        for (Observer<SwimmerStatus> obs : observers) {
+            obs.onEvent(event);
         }
     }
 }
